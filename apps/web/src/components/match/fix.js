8f8621ff078c6e75ae336fe8f78e26339f1342a1
Primananda <arielaryo4@gmail.com>
2026-05-29 20:50:29 +0700

    fix: score tracker bug

---
+++ apps/web/src/components/match/fix.js
@@ -0,0 +1,13 @@
+﻿const fs = require('fs');
+let content = fs.readFileSync('temp3.txt', 'utf8');
+
+// Find the second instance of handleSubtractPoint("team2")
+let index = content.indexOf('handleSubtractPoint("team2")');
+if (index !== -1) {
+  index = content.indexOf('handleSubtractPoint("team2")', index + 1);
+  if (index !== -1) {
+    content = content.substring(0, index) + 'handleSubtractPoint("team1")' + content.substring(index + 'handleSubtractPoint("team2")'.length);
+  }
+}
+
+fs.writeFileSync('ScoreTrackerClient.tsx', content, 'utf8');
