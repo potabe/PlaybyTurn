8ed90f68297bfc4fb8096f781df2b1afe1ad7700
Primananda <primananda0703@gmail.com>
2026-06-15 21:51:11 +0700

    fix: use Neon Serverless Adapter to fix Vercel PrismaClientInitializationError

--- apps/web/src/lib/prisma.ts
+++ apps/web/src/lib/prisma.ts
@@ -1,8 +1,16 @@
 import { PrismaClient } from "@prisma/client"
+import { Pool, neonConfig } from "@neondatabase/serverless"
+import { PrismaNeon } from "@prisma/adapter-neon"
+import ws from "ws"
+
+neonConfig.webSocketConstructor = ws
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient()
+const connectionString = `${process.env.DATABASE_URL || ""}`
+const pool = new Pool({ connectionString })
+const adapter = new PrismaNeon(pool as any)
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
